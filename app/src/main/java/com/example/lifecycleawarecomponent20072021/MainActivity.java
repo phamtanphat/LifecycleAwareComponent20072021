196b1adad5d45bd607f8b8079ac16bd9077ba8a2
phamtanphat <phatdroid94@gmail.com>
2021-10-26 18:17:39 +0700

    listener location

--- app/src/main/java/com/example/lifecycleawarecomponent20072021/MainActivity.java
+++ app/src/main/java/com/example/lifecycleawarecomponent20072021/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.lifecycleawarecomponent20072021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    MyLocationListener locationListener;
+    int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locationListener = new MyLocationListener(this);
+        getLifecycle().addObserver(locationListener);
+        locationListener.setOnListenerLocation(new MyLocationListener.OnListenLocation() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.d("BBB",count + " " +location.getLatitude()  + " " + location.getLongitude());
+                count++;
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/lifecycleawarecomponent20072021/MyLocationListener.java
+++ app/src/main/java/com/example/lifecycleawarecomponent20072021/MyLocationListener.java
@@ -1,25 +1,30 @@
 package com.example.lifecycleawarecomponent20072021;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.lifecycle.DefaultLifecycleObserver;
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleEventObserver;
 import androidx.lifecycle.LifecycleObserver;
 import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.OnLifecycleEvent;
 
-public class MyLocationListener implements DefaultLifecycleObserver  , LocationListener {
+public class MyLocationListener implements DefaultLifecycleObserver, LocationListener {
 
     Context context;
-
+    private OnListenLocation onListenLocation;
     private LocationManager locationManager;
 
-    public MyLocationListener(Context context){
+    public MyLocationListener(Context context) {
         this.context = context;
     }
 
@@ -27,21 +32,37 @@ public class MyLocationListener implements DefaultLifecycleObserver  , LocationL
     @Override
     public void onCreate(@NonNull LifecycleOwner owner) {
         locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0 , this);
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
     }
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
-
+        if (onListenLocation != null){
+            onListenLocation.onLocationChanged(location);
+        }
     }
 
     @Override
     public void onProviderEnabled(@NonNull String provider) {
-
     }
 
     @Override
     public void onProviderDisabled(@NonNull String provider) {
+    }
 
+    interface OnListenLocation{
+        void onLocationChanged(Location location);
+    }
+
+    public void setOnListenerLocation(OnListenLocation onListenerLocation){
+        this.onListenLocation = onListenerLocation;
+    }
+
+    @Override
+    public void onStop(@NonNull LifecycleOwner owner) {
+        locationManager.removeUpdates(this);
     }
 }
